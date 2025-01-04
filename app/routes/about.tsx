94477973ef6e3ef7bf724f1bf883695e9ca14846
Kyle Fidalgo <kyle.fidalgo@gmail.com>
2025-01-04 12:27:05 -0500

    Upgrade to tailwind v4

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,5 +1,5 @@
 export default function About() {
   return <main className="flex items-center justify-center pt-16 pb-4">
-    <h1>About</h1>
+    <h1 className="text-h1 font-bold">About</h1>
   </main>
 }
